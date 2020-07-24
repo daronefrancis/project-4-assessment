10b1fa27ba9a1adab40a4c154d77c19f17be57c5
daronefrancis <daronefrancis@gmail.com>
2020-07-24 19:03:57 -0400

    Updates

---
+++ precache-manifest.7f6be87c966a2eb2ce19a5d043e8a767.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57d4738236ecd6a31526dfa7ff78c10a",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "0e8ecf7a69e0a1481e41",
+    "url": "/project-4-assessment/static/css/main.48122197.chunk.css"
+  },
+  {
+    "revision": "acc314c7a357911f8cd2",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.0d68efe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e8ecf7a69e0a1481e41",
+    "url": "/project-4-assessment/static/js/main.9418720a.chunk.js"
+  },
+  {
+    "revision": "5f6f705e1e3b3b948cc9",
+    "url": "/project-4-assessment/static/js/runtime-main.3bb3fb77.js"
+  }
+]);
